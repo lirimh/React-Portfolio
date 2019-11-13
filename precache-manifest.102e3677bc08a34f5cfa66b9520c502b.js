a6166f5f8218b810730c880fa9c2f2fd854cf897
Lirim Hyseni <LirimH@blackstoneeit.com>
2019-11-13 10:12:53 +0100

    Updates

---
+++ precache-manifest.102e3677bc08a34f5cfa66b9520c502b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a22f76ba57526a898ceaa05cbd5d4c0e",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "8093670b3128b4bd5ea5",
+    "url": "/react-portfolio/static/css/2.3c1ddb0a.chunk.css"
+  },
+  {
+    "revision": "7a681e289cc78e9741cb",
+    "url": "/react-portfolio/static/css/main.ef448f5d.chunk.css"
+  },
+  {
+    "revision": "8093670b3128b4bd5ea5",
+    "url": "/react-portfolio/static/js/2.4629c641.chunk.js"
+  },
+  {
+    "revision": "7a681e289cc78e9741cb",
+    "url": "/react-portfolio/static/js/main.a3a746d1.chunk.js"
+  },
+  {
+    "revision": "16bb827c1c58b31a095c",
+    "url": "/react-portfolio/static/js/runtime-main.cc3dab56.js"
+  },
+  {
+    "revision": "3f5aeab6845047bc2f262a88bbf2a8f6",
+    "url": "/react-portfolio/static/media/photo1.3f5aeab6.jpg"
+  },
+  {
+    "revision": "11f6cfcb0c32e687421b04eecdc15c63",
+    "url": "/react-portfolio/static/media/photo2.11f6cfcb.jpg"
+  },
+  {
+    "revision": "869caaa3e5789a5c90b85be9d91c6fe9",
+    "url": "/react-portfolio/static/media/photo3.869caaa3.jpg"
+  },
+  {
+    "revision": "e957fcf8985e59701ae392e616a3c809",
+    "url": "/react-portfolio/static/media/profileFour.e957fcf8.png"
+  },
+  {
+    "revision": "dcfa5f60c4aa69b584f1441768edb3cf",
+    "url": "/react-portfolio/static/media/profileOne.dcfa5f60.png"
+  },
+  {
+    "revision": "53f5f724c93f32eec7dffc41af1b0a07",
+    "url": "/react-portfolio/static/media/profileThree.53f5f724.png"
+  },
+  {
+    "revision": "2a26e92ef6aa88b5f9984314044f959a",
+    "url": "/react-portfolio/static/media/profileTwo.2a26e92e.png"
+  }
+]);
